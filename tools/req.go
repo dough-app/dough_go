bcc586d7812e1d3cc04e9204bac8e343cb3585d7
ahyangnb <zonggeu@gmail.com>
2020-12-15 11:46:31 +0800

    commit ParseForm

--- tools/req.go
+++ tools/req.go
@@ -9,6 +9,10 @@ import (
 // 完美解析Post请求json参数
 // 支持：[x-www-from-urlencoded]、raw[json]
 func ReqPostBody(c *context.Context, key string) (value string, err error) {
+	err1 := c.Request.ParseForm()
+	if err1 != nil {
+		return "", err1
+	}
 	param := c.Request.Form[key]
 	if param != nil && len(param) != 0 {
 		fmt.Printf("param::%v\n", param)
@@ -18,9 +22,9 @@ func ReqPostBody(c *context.Context, key string) (value string, err error) {
 		// 用于存放参数key=value数据
 		var params map[string]string
 		// 解析参数 存入map
-		err := decoder.Decode(&params)
-		if err != nil {
-			return "", err
+		err2 := decoder.Decode(&params)
+		if err2 != nil {
+			return "", err2
 		}
 		return params[key], nil
 	}
